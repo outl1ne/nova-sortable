7c9f09b1f5ad3436d33f6896d0b765d23e2b9373
Tarvo R <tarvoreinpalu@gmail.com>
2022-08-19 15:27:55 +0300

    Fix console error stopPropagation

--- resources/js/icons/ChevronUpIcon.vue
+++ resources/js/icons/ChevronUpIcon.vue
@@ -6,7 +6,7 @@
     height="12"
     class="fill-current outline-none"
     :class="customClass"
-    @click="$emit('click')"
+    @click="e => $emit('click', e)"
   >
     <path
       d="M13 5.41V21a1 1 0 0 1-2 0V5.41l-5.3 5.3a1 1 0 1 1-1.4-1.42l7-7a1 1 0 0 1 1.4 0l7 7a1 1 0 1 1-1.4 1.42L13 5.4z"
