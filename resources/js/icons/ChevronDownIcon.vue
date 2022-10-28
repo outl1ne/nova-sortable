ba54222fadecd189874e034ae0f6f865a1d68ef3
Tarvo R <tarvoreinpalu@gmail.com>
2022-10-28 12:57:17 +0300

    Fix double reorder warning

--- resources/js/icons/ChevronDownIcon.vue
+++ resources/js/icons/ChevronDownIcon.vue
@@ -6,7 +6,6 @@
     height="12"
     class="fill-current outline-none"
     :class="customClass"
-    @click="e => $emit('click', e)"
   >
     <path
       d="M11 18.59V3a1 1 0 0 1 2 0v15.59l5.3-5.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-7-7a1 1 0 0 1 1.4-1.42l5.3 5.3z"
