edd285cf55ee1451a0f5b4b9cd4188389c936091
Tarvo R <tarvoreinpalu@gmail.com>
2023-03-17 15:25:06 +0200

    Fix theme handling on "System"

--- resources/js/entry.js
+++ resources/js/entry.js
@@ -3,24 +3,27 @@ import ResourceTableHeader from './components/ResourceTableRow';
 import ResourceTableRow from './components/ResourceTableRow';
 import ReorderButtons from './components/ReorderButtons';
 
+const handleDarkMode = () => {
+  const cls = document.documentElement.classList;
+  const isDarkMode = cls.contains('dark');
+
+  if (isDarkMode && !cls.contains('o1-dark')) {
+    cls.add('o1-dark');
+  } else if (!isDarkMode && cls.contains('o1-dark')) {
+    cls.remove('o1-dark');
+  }
+};
+
 Nova.booting((app, router, store) => {
-  app.component('ResourceTable', require('./components/ResourceTable').default);
-  app.component('ResourceTableHeader', require('./components/ResourceTableHeader').default);
-  app.component('ResourceTableRow', require('./components/ResourceTableRow').default);
-  app.component('ReorderButtons', require('./components/ReorderButtons').default);
-
-  new MutationObserver(() => {
-    const cls = document.documentElement.classList;
-    const isDarkMode = cls.contains('dark');
-
-    if (isDarkMode && !cls.contains('o1-dark')) {
-      cls.add('o1-dark');
-    } else if (!isDarkMode && cls.contains('o1-dark')) {
-      cls.remove('o1-dark');
-    }
-  }).observe(document.documentElement, {
+  handleDarkMode();
+  new MutationObserver(handleDarkMode).observe(document.documentElement, {
     attributes: true,
     attributeOldValue: true,
     attributeFilter: ['class'],
   });
+
+  app.component('ResourceTable', ResourceTable);
+  app.component('ResourceTableHeader', ResourceTableHeader);
+  app.component('ResourceTableRow', ResourceTableRow);
+  app.component('ReorderButtons', ReorderButtons);
 });
