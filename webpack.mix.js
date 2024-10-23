61f2ba241d74036fc131cc7b8f0905ef00ad2648
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-23 16:12:16 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,15 @@
 let mix = require('laravel-mix');
 let path = require('path');
 
-require('./nova.mix');
+mix.extend('nova', new require('./vendor/laravel/nova-devtool'))
 
 mix
   .setPublicPath('dist')
   .js('resources/js/entry.js', 'js')
   .vue({ version: 3 })
   .postCss('resources/css/tool.css', 'css', [require('tailwindcss')])
+  .alias({
+    'laravel-nova-mixins': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/index.js'),
+    '@': path.join(__dirname, 'vendor/laravel/nova/resources/js/'),
+  })
   .nova('outl1ne/nova-sortable');
