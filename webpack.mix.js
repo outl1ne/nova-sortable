767616c6d91908b7dca41fdf6ece90f31efad1bc
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-23 19:02:32 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,8 +8,4 @@ mix
   .js('resources/js/entry.js', 'js')
   .vue({ version: 3 })
   .postCss('resources/css/tool.css', 'css', [require('tailwindcss')])
-  .alias({
-    'laravel-nova-mixins': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/index.js'),
-    '@': path.join(__dirname, 'vendor/laravel/nova/resources/js/'),
-  })
   .nova('outl1ne/nova-sortable');
